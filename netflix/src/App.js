4c2fce6830da87175d6c374c7907cd5a085766d5
komal <komalpal0000@gmail.com>
2021-08-15 14:17:36 +0530

    Update App.js

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,6 +1,4 @@
 import React from "react";
-// import Card from './Cards';
-// import Cdata from './Cdata';
 import Netflix from "./Netflix";
 import Amazone from "./Amazone";
 const fav = 'netfli';
@@ -8,57 +6,17 @@ const FavS = () => {
   // console.log(Cdata[1]);
 if(fav==='netflix')
 {
-  // return(
-  //   <Card
-  //         // keys={val.id}-error bcs you can only write key
-  //         key={Cdata[1].id}
-  //         imgscr={Cdata[1].imgscr} 
-  //         category={Cdata[1].category} 
-  //         title={Cdata[1].title} 
-  //         link={Cdata[1].link} 
-    
-  //   />
-    
-  //     );
-
  return <Netflix />;
 }
 else{
-  // return(
-  //   <Card
-  //         // keys={val.id}-error bcs you can only write key
-  //         key={Cdata[3].id}
-  //         imgscr={Cdata[3].imgscr} 
-  //         category={Cdata[3].category} 
-  //         title={Cdata[3].title} 
-  //         link={Cdata[3].link} 
-    
-  //   />
-    
-  //     );
+  
   return <Amazone />
 }
 };
 const App = () => (
   <>
     <h1 className="heading">Top Netflix Series</h1>
-    {/* {Cdata.map((val,index) => {
-      // console.log(index);
-      return(
-<Card
-      // keys={val.id}-error bcs you can only write key
-      key={val.id}
-      imgscr={val.imgscr} 
-      category={val.category} 
-      title={val.title} 
-      link={val.link} 
 
-/>
-
-  ); 
-
-
-})}*/}
 <FavS />
 
 {/* Using ternary operator */}
